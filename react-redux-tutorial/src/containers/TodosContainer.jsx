abbfab2e7c0680a7d94e95b3237e032d6a2f1d25
rlalasrl <rlaalsrl0810@naver.com>
2020-08-25 22:39:27 +0900

    lib/actions fucntion

--- react-redux-tutorial/src/containers/TodosContainer.jsx
+++ react-redux-tutorial/src/containers/TodosContainer.jsx
@@ -2,17 +2,23 @@ import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
+import useAction from '../lib/useAction';
 
 const TodoContainer = () => {
   const { input, todos } = useSelector(({ todos }) => ({
     input: todos.input,
     todos: todos.todos
   }));
-  const dispatch = useDispatch();
-  const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
-  const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
-  const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
-  const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+  // const dispatch = useDispatch();
+  // const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
+  // const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
+  // const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
+  // const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useAction(
+    [changeInput, insert, toggle, remove],
+    []
+  );
 
   return (
     <Todos 
@@ -20,13 +26,13 @@ const TodoContainer = () => {
       todos={todos}
       onChangeInput={onChangeInput}
       onInsert={onInsert}
-      onToggle={ontoggle}
+      onToggle={onToggle}
       onRemove={onRemove}
     />
   )
 }
 
-export default TodoContainer;
+export default React.memo(TodoContainer);
 
 // const TodoContainer = ({
 //   input,
