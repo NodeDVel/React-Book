628eda12066396af72a2c427c4fc6c151d72be1b
rlalasrl <rlaalsrl0810@naver.com>
2020-08-25 22:27:02 +0900

    todos-redux HOOKS

--- react-redux-tutorial/src/containers/TodosContainer.jsx
+++ react-redux-tutorial/src/containers/TodosContainer.jsx
@@ -1,37 +1,62 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
 
-const TodoContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+const TodoContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos
+  }));
+  const dispatch = useDispatch();
+  const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
+  const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
+  const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
+  const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+
   return (
     <Todos 
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={ontoggle}
+      onRemove={onRemove}
     />
-  );
-};
+  )
+}
 
-export default connect(
-  ({todos}) => ({
-    input: todos.input,
-    todos: todos.todos,
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove,
-  }
-)(TodoContainer);
+export default TodoContainer;
+
+// const TodoContainer = ({
+//   input,
+//   todos,
+//   changeInput,
+//   insert,
+//   toggle,
+//   remove,
+// }) => {
+//   return (
+//     <Todos 
+//       input={input}
+//       todos={todos}
+//       onChangeInput={changeInput}
+//       onInsert={insert}
+//       onToggle={toggle}
+//       onRemove={remove}
+//     />
+//   );
+// };
+
+// export default connect(
+//   ({todos}) => ({
+//     input: todos.input,
+//     todos: todos.todos,
+//   }),
+//   {
+//     changeInput,
+//     insert,
+//     toggle,
+//     remove,
+//   }
+// )(TodoContainer);
